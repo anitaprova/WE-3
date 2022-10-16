dbb04a551076830cedae84d922f422b4e8c436d4
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-16 19:33:29 -0400

    done

--- 3.1.cpp
+++ 3.1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(){
+	string str;
+	cin >> str;
+	string result;
+
+	if (str.length() % 2 != 0){
+		result = str.substr(str.length()/2, 1);
+	}
+	else {
+		result = str.substr((str.length()-1)/2, 2);
+	}
+	cout << result << endl;
+	
+	return 0;
+}
